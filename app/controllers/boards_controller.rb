8d9db8970685dc4155096fe7ada10f00ed1529c0
Hal Dunn <heyhal19@gmail.com>
2021-03-31 12:05:46 -0700

    adds create/new methods to controller

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,10 +2,30 @@ class BoardsController < ApplicationController
     def index
         @boards = Board.all
     end
+    
+    def create
+        @newBoard = Board.new(board_params)
+        if @newBoard.save
 
-    def show
-        @board = Boar.dfind(params[:id])
+            redirect_to @newBoard
+        else
+            # add errors info to flash
+        end
+        # this should actually redirect to show but leaving as index for now
+        # fallback, re-render same page
+        # render :new
     end
 
-    # should add strong params
+    def new
+        @board = Board.new
+    end
+
+    def show
+        @board = Board.find(params[:id])
+    end
+
+    private
+    def board_params
+        params.require(:board).permit(:name, :email, :height, :width, :mines)
+      end
 end
