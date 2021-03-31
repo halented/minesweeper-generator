d7a96b8f280be87ee27b0890e95f0513c6f2b66c
Hal Dunn <heyhal19@gmail.com>
2021-03-31 12:32:23 -0700

    adds linking for index and show page

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,6 +2,9 @@ class Board < ApplicationRecord
     # all attributes must exist
     validates :name, :email, :height, :width, :mines, presence: true
 
+    # board name must be unique
+    validates :name, uniqueness: true
+
     # board values cannot be negative, 0, or a float
     validates :height, :width, :mines, numericality: {only_integer: true, greater_than_or_equal_to: 1}
 
