ffcbc8619601e5ee8cf8749ef146a770f08c5de6
Hal Dunn <heyhal19@gmail.com>
2021-04-02 13:12:19 -0700

    base styles for index, new, show

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   resources :boards, only: [:index, :show, :new, :create]
 
   get '/', :to => 'boards#new'
+
+  if Rails.env.production?
+    get '404', :to => 'application#page_not_found'
+  end
+
 end
